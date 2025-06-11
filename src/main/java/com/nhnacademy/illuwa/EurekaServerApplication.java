5c8572d7014e393924660a35bcd271c1e7adc23d
pear_c <zxcv9676@naver.com>
2025-06-11 09:28:42 +0900

    feat: Eureka 의존성 추가 및 설정

--- src/main/java/com/nhnacademy/illuwa/EurekaServerApplication.java
+++ src/main/java/com/nhnacademy/illuwa/EurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.nhnacademy.illuwa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class IlluwaApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IlluwaApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
